0e37977a7bba4786daf672654b8eb3dc492d61c9
LorMeng Song2003 <lormengsong@gmail.com>
2022-12-22 15:10:10 +0700

    crud

---
+++ tests/Fixture/CategoriesFixture.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * CategoriesFixture
+ */
+class CategoriesFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'name' => 'Lorem ipsum dolor sit amet',
+                'created_at' => 1671695401,
+                'updated_at' => 1671695401,
+            ],
+        ];
+        parent::init();
+    }
+}
