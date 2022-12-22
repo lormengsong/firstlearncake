05bebb53d3b8d5632b41166fc0a160ebbbc08246
LorMeng Song2003 <lormengsong@gmail.com>
2022-12-22 14:17:10 +0700

    first commit

---
+++ tests/Fixture/StudentsFixture.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * StudentsFixture
+ */
+class StudentsFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'name' => 'Lorem ipsum dolor sit amet',
+                'age' => 1,
+                'country' => 'Lorem ipsum dolor sit amet',
+                'created_at' => 1671641826,
+                'updated_at' => 1671641826,
+            ],
+        ];
+        parent::init();
+    }
+}
